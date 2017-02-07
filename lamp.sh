dec45452ea6916d455143720dab9c96fff117896
Ryan Antenor <contact@ryanantenor.be>
2017-02-07 20:55:18 +0100

    enable php7.0 module

--- lamp.sh
+++ lamp.sh
@@ -9,7 +9,10 @@ sudo apt-get update
 
 #install Apache, Mysql and Apache
 
-sudo apt-get install -y  php7.0-cli php7.0-common libapache2-mod-php7.0 php7.0 php7.0-mysql php7.0-fpm php7.0-curl php7.0-gd php7.0-bz2 apache2
+sudo apt-get install -y  php7.0-mbstring libapache2-mod-php php7.0-cli php7.0-common libapache2-mod-php7.0 php7.0 php7.0-mysql php7.0-fpm php7.0-curl php7.0-gd php7.0-bz2 apache2
+
+#Enale php7.0 module
+sudo a2enmod php7.0
 
 echo "########################################################"
 echo "		Setup MYSQL - Password: 'root'                "
@@ -19,12 +22,12 @@ echo "########################################################"
 
 echo "mysql-server-5.7 mysql-server/root_password password root" | debconf-set-selections
 echo "mysql-server-5.7 mysql-server/root_password_again password root" | debconf-set-selections
-apt-get -y install mysql-server-5.7
+sudo apt-get -y install mysql-server-5.7
 
 #Restart all services
 echo -e "\n"
 
-service apache2 restart && service mysql restart > /dev/null
+sudo service apache2 restart && service mysql restart > /dev/null
 
 echo -e "\n"
 
