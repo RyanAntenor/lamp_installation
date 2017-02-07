9dcaedaf17ef6697fd76feeb272058e4ba4785bc
Ryan Antenor <contact@ryanantenor.be>
2017-02-07 04:14:06 +0100

    lamp installation for ubuntu

---
+++ README.md
@@ -0,0 +1,31 @@
+Automated-LAMP-Installation
+===========================
+
+This shell script will install the LAMP Stack interactively on Ubuntu server
+
+
+It will install
+--------------
+- Mysql
+- Apache2
+- Php 7.0
+
+
+How to use it:
+-------------
+
+Download the following shell script on your Ubuntu server or clone it with the following command.
+```
+git clone https://github.com/RyanAntenor/lamp_installation.git
+```
+Give the execution right to it.
+------------------------------
+```
+chmod +x lamp.sh
+```
+Then run it as sudo user.
+-------------------------
+```
+sudo ./lamp.sh
+```
+
---
+++ lamp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "########################################################"
+echo "           Installing LAMP Service                      "
+echo "########################################################"
+
+#update the repository
+sudo apt-get update
+
+#install Apache, Mysql and Apache
+
+sudo apt-get install -y  php7.0-cli php7.0-common libapache2-mod-php7.0 php7.0 php7.0-mysql php7.0-fpm php7.0-curl php7.0-gd php7.0-bz2 apache2
+
+echo "########################################################"
+echo "		Setup MYSQL - Password: 'root'                "
+echo "########################################################"
+
+#The following commands set the MySQL root password to root when you install the mysql-server package.
+
+echo "mysql-server-5.7 mysql-server/root_password password root" | debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password root" | debconf-set-selections
+apt-get -y install mysql-server-5.7
+
+#Restart all services
+echo -e "\n"
+
+service apache2 restart && service mysql restart > /dev/null
+
+echo -e "\n"
+
+if [ $? -ne 0 ]; then
+   echo "Please Check the Install Services, There is some $(tput bold)$(tput setaf 1)Problem$(tput sgr0)"
+else
+   echo "Installed Services run $(tput bold)$(tput setaf 2)Sucessfully$(tput sgr0)"
+fi
+
+echo -e "\n"
